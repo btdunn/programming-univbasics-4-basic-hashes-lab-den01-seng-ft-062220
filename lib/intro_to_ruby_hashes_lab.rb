450c8e5e88a24c6fe57aa68489cd8bcb2e4e3f0a
interactive-directory-1839 <benthedunn@gmail.com>
2020-05-22 20:45:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -13,10 +13,12 @@ def pioneer
   ok = {
     :name => "Grace Hopper"
   }
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+  ok = {
+    :id => number
+  }
   # return a hash with a key :id assigned to the provided number
 end
